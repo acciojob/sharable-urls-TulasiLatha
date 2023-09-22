b0cb896dc7881be8011143b44db81d450f74e0f8
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-09-22 11:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const urlForm = document.getElementById('urlForm');
+        const nameInput = document.getElementById('name');
+        const yearInput = document.getElementById('year');
+        const urlOutput = document.getElementById('url');
+
+urlForm.addEventListener('submit',function(event){
+	event.preventdefault();
+	const name=document.getElementById('name').value;
+	const year=document.getElementById('year').value;
+
+	let queryString =`?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+urlOutput.textContent = `https://localhost:8080/${queryString}`;
+}
+						)
